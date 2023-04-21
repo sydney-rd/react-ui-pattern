fd180eedd6372ba6e786ce79f53096312ee4d793
sydney-rd <sydneydavid56@gmail.com>
2023-04-21 11:27:02 -0400

    adding functionality for limiting search results

--- src/components/Stamp.jsx
+++ src/components/Stamp.jsx
@@ -12,7 +12,6 @@ function Stamp({ country, onClick }) {
             </div>
 
         </div>
-
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,27 @@ import Modal from "../components/Modal";
 
 // fetch request that happens everytime the homepage is loaded
 function Home() {
-    // js
     const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState("oceania")
     const [countryModal, setCountryModal] = useState({})
     const [toggle, setToggle] = useState(false)
 
-    useEffect(() => {
-        const getCountries = async () => {
-            const response = await axios.get(
-                "https://restcountries.com/v3.1/region/oceania"
-            )
-            setCountries(response.data)
+    const getCountries = async () => {
+        const response = await axios.get(
+            `https://restcountries.com/v3.1/region/${search}`
+        )
+
+        let limit = 15 
+        let results = response.data
+
+        if (results.length > limit) {
+            results.length = 15
         }
+
+        setCountries(results)
+    }
+
+    useEffect(() => {
         getCountries();
     }, [])
 
