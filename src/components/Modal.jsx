3a83a8034243753341b0a1ebc13529f31b0c4187
sydney-rd <sydneydavid56@gmail.com>
2023-04-24 10:54:01 -0400

    pls

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,20 @@
+function Modal({ country }) {
+  function name() {
+    return country && country.name ? country.name.common : "";
+  }
 
-function Modal( {country} ) {
-    function name() {
-        return country && country.name ? country.name.common : "";    }
-    
-    return(
-
-        <div className = "modal">
-            <p> I just traveled to {name()} check this out...</p>
-            <p> The capital here is {country.capital}! </p>
-            <p> The population in {name()} is {country.population} </p>
-            <p> You'll find this country in {country.region} </p>
-            <p> How cool is their flag? {country.flag} </p>
-        </div>
-    )
+  return (
+    <div className="modal">
+      <p> I just traveled to {name()} check this out...</p>
+      <p> The capital here is {country.capital}! </p>
+      <p>
+        {" "}
+        The population in {name()} is {country.population}{" "}
+      </p>
+      <p> You'll find this country in {country.region} </p>
+      <p> How cool is their flag? {country.flag} </p>
+    </div>
+  );
 }
 
-export default Modal
+export default Modal;
--- src/components/Stamp.jsx
+++ src/components/Stamp.jsx
@@ -1,11 +1,15 @@
 function Stamp({ country, onClick }) {
-    return (
-        <div className="stamp-container" onClick={onClick}>
-            <div className="stamp">
-                <img className="stamp-flag" src={country.flags.png} alt={country.name.common} /> 
-            </div>
-        </div>
-    )
+  return (
+    <div className="stamp-container" onClick={onClick}>
+      <div className="stamp">
+        <img
+          className="stamp-flag"
+          src={country.flags.png}
+          alt={country.name.common}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Stamp;
