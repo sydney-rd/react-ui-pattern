8d68bf5839aaad83e984f9926099b78a71ffbf03
sydney-rd <sydneydavid56@gmail.com>
2023-04-24 10:16:34 -0400

    adding formatting

--- src/components/Stamp.jsx
+++ src/components/Stamp.jsx
@@ -1,16 +1,8 @@
-import { Link, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 function Stamp({ country, onClick }) {
-
     return (
         <div className="stamp-container" onClick={onClick}>
-
             <div className="stamp">
-                <img className="stamp-flag" src={country.flags.png} alt={country.name.common} />
-                
-
+                <img className="stamp-flag" src={country.flags.png} alt={country.name.common} /> 
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Link, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Stamp from "../components/Stamp";
@@ -6,75 +5,74 @@ import Modal from "../components/Modal";
 
 // fetch request that happens everytime the homepage is loaded
 function Home() {
-    const [countries, setCountries] = useState([])
-    const [search, setSearch] = useState("oceania") // name of region that starts with useeffect
-    const [countryModal, setCountryModal] = useState({})
-    const [toggle, setToggle] = useState(false)
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("oceania"); // name of region that starts with useeffect
+  const [countryModal, setCountryModal] = useState({});
+  const [toggle, setToggle] = useState(false);
 
-    const getCountries = async () => {
-        const response = await axios.get(
-            `https://restcountries.com/v3.1/region/${search}`
-        )
-        let limit = 15 
-        let results = response.data
+  const getCountries = async () => {
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/region/${search}`
+    );
+    let limit = 15;
+    let results = response.data;
 
-        if (results.length > limit) {
-            results.length = 15
-        }
-        setCountries(results)
-        setSearch("")
+    if (results.length > limit) {
+      results.length = 15;
     }
+    setCountries(results);
+    setSearch("");
+  };
 
-    useEffect(() => {
-        getCountries();
-    }, [])
+  useEffect(() => {
+    getCountries();
+  }, []);
 
-    function modalPopUp(country) {
-        console.log(country)
-        if (toggle) {
-            setToggle(false)
-            setCountryModal({});
-        } else {
-            setToggle(true)
-            setCountryModal(country);
-        }
-    }   
-
-    function modalExit(country) {
-        
+  function modalPopUp(country) {
+    console.log(country);
+    if (toggle) {
+      setToggle(false);
+      setCountryModal({});
+    } else {
+      setToggle(true);
+      setCountryModal(country);
     }
+  }
 
-    // handle input change for searching regions
-    function handleInputChange() {
-        getCountries()
-    }
+  function modalExit(country) {}
 
-    // console.log(countries); // console logs oceanic countries
-    return (
-        <div>
-          <div className="search-bar-container">
-            <input 
-            className="search-bar" 
-            type="text" 
-            placeholder="region of the world"
-            onChange={(e) => setSearch(e.target.value)} 
-            value={search}/>
-            <button onClick={handleInputChange}>Click</button>
-          </div>
-          <div className="home-container">
-            {toggle && <Modal country={countryModal}/>} 
-            {countries.map((country, idx) => {
-              return (
-                <Stamp 
-                  country={country} 
-                  key={idx} 
-                  onClick={() => modalPopUp(country)} 
-                />
-              )
-            })}
-          </div>
-        </div>
-      )
-}      
+  // handle input change for searching regions
+  function handleInputChange() {
+    getCountries();
+  }
+
+  // console.log(countries); // console logs oceanic countries
+  return (
+    <div>
+      <div className="search-bar-container">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="region of the world"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button onClick={handleInputChange}>Click</button>
+      </div>
+      <div className="home-container">
+        {toggle && <Modal country={countryModal} />}
+        {countries.map((country, idx) => {
+          return (
+            <Stamp
+              country={country}
+              key={idx}
+              onClick={() => modalPopUp(country)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Home;
